42dc2978aa6c6160c42649b39dd870909a3b4202
Alexander Shein <mr.ashein@gmail.com>
2020-06-07 15:00:03 +0300

    Processed fasta files with DNA punctatioan, added shell scripts

---
+++ src/run_search_pal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# First arg: target path
+
+COUNTER=0
+LEN=$(ls $1/*fasta | wc -l)
+for filename in $(ls $1 | head -n$LEN); do
+    ./src/dnapunctuation $1/$filename 10 20 0 8 2
+    COUNTER=$[COUNTER + 1]
+    if (($(($COUNTER%5)) == 0))
+    then
+        echo -ne "\r#Processing $COUNTER out of $LEN"
+    fi
+done;
+echo -ne "\r#Done, processed $COUNTER files\n"

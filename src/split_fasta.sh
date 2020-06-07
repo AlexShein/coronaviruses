ce59f68a5e05db45dd257aaab8137ee53d34f15c
Alexander Shein <mr.ashein@gmail.com>
2020-06-07 11:26:04 +0300

    Added raw dataset, shell fasta-splitting script and processed fasta files

---
+++ src/split_fasta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# $1 - first arg - path to source fasta file with multiple sequences
+# $2 - second arg - path to folder with processed fasta results
+
+RECORDS_COUNT=$(($(cat $1 | grep -c '>')-2))
+
+csplit $1 '/^>.*/' "{$RECORDS_COUNT}"
+
+for filename in $(ls xx*); do
+    SEQ_NAME=$(
+        head -n1 $filename | sed -e 's/^.*\.[0-9] \(.*\)$/\1/' -e 's/[ \/]/_/g' -e 's/,//g'
+        )
+    echo "Moving $filename to $2/$SEQ_NAME.fasta"
+    mv $filename $2/$SEQ_NAME.fasta
+done;
+echo "Done!"
